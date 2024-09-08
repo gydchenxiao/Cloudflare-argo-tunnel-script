8933758e50c99c12db376f0a9d57a59e180fb6cd
小咕咚 <122521272+gydchenxiao@users.noreply.github.com>
2024-09-08 23:53:18 +0800

    Update argo.sh
    删掉一键安装模式的代码，生成临时cloudflare argo quick tunnel

--- argo.sh
+++ argo.sh
@@ -38,171 +38,6 @@ then
 	fi
 fi
 
-
-function quicktunnel(){
-rm -rf xray cloudflared-linux xray.zip
-case "$(uname -m)" in
-	x86_64 | x64 | amd64 )
-	curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o xray.zip
-	curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -o cloudflared-linux
-	;;
-	i386 | i686 )
-	curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-32.zip -o xray.zip
-	curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386 -o cloudflared-linux
-	;;
-	armv8 | arm64 | aarch64 )
-	echo arm64
-	curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip -o xray.zip
-	curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -o cloudflared-linux
-	;;
-	armv7l )
-	curl -L https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm32-v7a.zip -o xray.zip
-	curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm -o cloudflared-linux
-	;;
-	* )
-	echo 当前架构$(uname -m)没有适配
-	exit
-	;;
-esac
-mkdir xray
-unzip -d xray xray.zip
-chmod +x cloudflared-linux xray/xray
-rm -rf xray.zip
-uuid=$(cat /proc/sys/kernel/random/uuid)
-urlpath=$(echo $uuid | awk -F- '{print $1}')
-port=$[$RANDOM+10000]
-if [ $protocol == 1 ]
-then
-cat>xray/config.json<<EOF
-{
-	"inbounds": [
-		{
-			"port": $port,
-			"listen": "localhost",
-			"protocol": "vmess",
-			"settings": {
-				"clients": [
-					{
-						"id": "$uuid",
-						"alterId": 0
-					}
-				]
-			},
-			"streamSettings": {
-				"network": "ws",
-				"wsSettings": {
-					"path": "$urlpath"
-				}
-			}
-		}
-	],
-	"outbounds": [
-		{
-			"protocol": "freedom",
-			"settings": {}
-		}
-	]
-}
-EOF
-fi
-if [ $protocol == 2 ]
-then
-cat>xray/config.json<<EOF
-{
-	"inbounds": [
-		{
-			"port": $port,
-			"listen": "localhost",
-			"protocol": "vless",
-			"settings": {
-				"decryption": "none",
-				"clients": [
-					{
-						"id": "$uuid"
-					}
-				]
-			},
-			"streamSettings": {
-				"network": "ws",
-				"wsSettings": {
-					"path": "$urlpath"
-				}
-			}
-		}
-	],
-	"outbounds": [
-		{
-			"protocol": "freedom",
-			"settings": {}
-		}
-	]
-}
-EOF
-fi
-./xray/xray run>/dev/null 2>&1 &
-./cloudflared-linux tunnel --url http://localhost:$port --no-autoupdate --edge-ip-version $ips --protocol http2 >argo.log 2>&1 &
-sleep 1
-n=0
-while true
-do
-n=$[$n+1]
-clear
-echo 等待cloudflare argo生成地址 已等待 $n 秒
-argo=$(cat argo.log | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}')
-if [ $n == 15 ]
-then
-	n=0
-	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
-	then
-		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $1}') >/dev/null 2>&1
-	else
-		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $2}') >/dev/null 2>&1
-	fi
-	rm -rf argo.log
-	clear
-	echo argo获取超时,重试中
-	./cloudflared-linux tunnel --url http://localhost:$port --no-autoupdate --edge-ip-version $ips --protocol http2 >argo.log 2>&1 &
-	sleep 1
-elif [ -z "$argo" ]
-then
-	sleep 1
-else
-	rm -rf argo.log
-	break
-fi
-done
-clear
-if [ $protocol == 1 ]
-then
-	echo -e vmess链接已经生成, visa.cn 可替换为CF优选IP'\n' > v2ray.txt
-	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
-	then
-		echo 'vmess://'$(echo '{"add":"visa.cn","aid":"0","host":"'$argo'","id":"'$uuid'","net":"ws","path":"'$urlpath'","port":"443","ps":"'$(echo $isp | sed -e 's/_/ /g')'_tls","tls":"tls","type":"none","v":"2"}' | base64 | awk '{ORS=(NR%76==0?RS:"");}1') >> v2ray.txt
-	else
-		echo 'vmess://'$(echo '{"add":"visa.cn","aid":"0","host":"'$argo'","id":"'$uuid'","net":"ws","path":"'$urlpath'","port":"443","ps":"'$(echo $isp | sed -e 's/_/ /g')'_tls","tls":"tls","type":"none","v":"2"}' | base64 -w 0) >> v2ray.txt
-	fi
-	echo -e '\n'端口 443 可改为 2053 2083 2087 2096 8443'\n' >> v2ray.txt
-	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
-	then
-		echo 'vmess://'$(echo '{"add":"visa.cn","aid":"0","host":"'$argo'","id":"'$uuid'","net":"ws","path":"'$urlpath'","port":"80","ps":"'$(echo $isp | sed -e 's/_/ /g')'","tls":"","type":"none","v":"2"}' | base64 | awk '{ORS=(NR%76==0?RS:"");}1') >> v2ray.txt
-	else
-		echo 'vmess://'$(echo '{"add":"visa.cn","aid":"0","host":"'$argo'","id":"'$uuid'","net":"ws","path":"'$urlpath'","port":"80","ps":"'$(echo $isp | sed -e 's/_/ /g')'","tls":"","type":"none","v":"2"}' | base64 -w 0) >> v2ray.txt
-	fi
-	echo -e '\n'端口 80 可改为 8080 8880 2052 2082 2086 2095 >> v2ray.txt
-fi
-if [ $protocol == 2 ]
-then
-	echo -e vless链接已经生成, visa.cn 可替换为CF优选IP'\n' > v2ray.txt
-	echo 'vless://'$uuid'@visa.cn:443?encryption=none&security=tls&type=ws&host='$argo'&path='$urlpath'#'$(echo $isp | sed -e 's/_/%20/g' -e 's/,/%2C/g')'_tls' >> v2ray.txt
-	echo -e '\n'端口 443 可改为 2053 2083 2087 2096 8443'\n' >> v2ray.txt
-	echo 'vless://'$uuid'@visa.cn:80?encryption=none&security=none&type=ws&host='$argo'&path='$urlpath'#'$(echo $isp | sed -e 's/_/%20/g' -e 's/,/%2C/g')'' >> v2ray.txt
-	echo -e '\n'端口 80 可改为 8080 8880 2052 2082 2086 2095 >> v2ray.txt
-fi
-rm -rf argo.log
-cat v2ray.txt
-echo -e '\n'信息已经保存在 v2ray.txt,再次查看请运行 cat v2ray.txt
-}
-
 function installtunnel(){
 #创建主目录
 mkdir -p /opt/cf/ >/dev/null 2>&1
@@ -628,58 +463,22 @@ ln -sf /opt/cf/cf.sh /usr/bin/cf
 }
 
 clear
-echo 一键安装模式不需要自己提供域名,使用CF ARGO QUICK TUNNEL创建快速链接临时隧道
-echo 一键安装模式在重启或者脚本再次运行后失效,如果需要使用需要再次运行创建
-echo 安装服务模式,需要有CF托管域名,并且需要按照提示手动绑定ARGO服务
-echo 首次绑定ARGO服务后如果不想再次跳转网页绑定
+echo 安装服务模式,需要有Cloudflare托管域名,并且需要按照提示手动绑定argo服务
+echo 首次绑定argO服务后如果不想再次跳转网页绑定
 echo 将已经绑定的系统目录下的 /root/.cloudflared 文件夹以及内容
 echo 拷贝至新系统下同样的目录,会自动跳过登录验证
 
-echo -e '\n'Cloudlfare argo tunnel一键安装'\n'
+echo -e '\n'Cloudlfare argo tunnel一键安装脚本'\n'
 echo 1.一键安装模式
-echo 2.安装服务
-echo 3.卸载服务
-echo 4.清空缓存
+echo 2.卸载服务
+echo 3.清空缓存
 echo -e 0.退出脚本'\n'
-read -p "请选择模式(默认1):" mode
+read -p "请选择:" mode
 if [ -z "$mode" ]
 then
 	mode=1
 fi
-if [ $mode == 1 ]
-then
-	read -p "请选择xray协议(默认1.vmess,2.vless):" protocol
-	if [ -z "$protocol" ]
-	then
-		protocol=1
-	fi
-	if [ $protocol != 1 ] && [ $protocol != 2 ]
-	then
-		echo 请输入正确的xray协议
-		exit
-	fi
-	read -p "请选择argo连接模式IPV4或者IPV6(输入4或6,默认4):" ips
-	if [ -z "$ips" ]
-	then
-		ips=4
-	fi
-	if [ $ips != 4 ] && [ $ips != 6 ]
-	then
-		echo 请输入正确的argo连接模式
-		exit
-	fi
-	isp=$(curl -$ips -s https://visa.cn/meta | awk -F\" '{print $26"-"$18"-"$30}' | sed -e 's/ /_/g')
-	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
-	then
-		kill -9 $(ps -ef | grep xray | grep -v grep | awk '{print $1}') >/dev/null 2>&1
-		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $1}') >/dev/null 2>&1
-	else
-		kill -9 $(ps -ef | grep xray | grep -v grep | awk '{print $2}') >/dev/null 2>&1
-		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $2}') >/dev/null 2>&1
-	fi
-	rm -rf xray cloudflared-linux v2ray.txt
-	quicktunnel
-elif [ $mode == 2 ]
+elif [ $mode == 1 ]
 then
 	read -p "请选择xray协议(默认1.vmess,2.vless):" protocol
 	if [ -z "$protocol" ]
@@ -720,7 +519,7 @@ then
 	installtunnel
 	cat /opt/cf/v2ray.txt
 	echo 服务安装完成,管理服务请运行命令 cf
-elif [ $mode == 3 ]
+elif [ $mode == 2 ]
 then
 	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
 	then
@@ -742,7 +541,7 @@ then
 	echo 彻底删除授权记录
 	echo 请访问 https://dash.cloudflare.com/profile/api-tokens
 	echo 删除授权的 Argo Tunnel API Token 即可
-elif [ $mode == 4 ]
+elif [ $mode == 3 ]
 then
 	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
 	then
